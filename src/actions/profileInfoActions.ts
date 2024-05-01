6b150fd38da5ec22714b42fc6f7b894a93cc34a4
Edyth-K <kdela@ryerson.ca>
2024-05-01 15:20:41 -0500

    finished profile info form connect
    saving profile now saves user/displayname/bio to database

--- src/actions/profileInfoActions.ts
+++ src/actions/profileInfoActions.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 
 export async function saveProfile(formData: FormData) {
+    // connect db
     await mongoose.connect(process.env.MONGODB_URI as string);
     const session = await getServerSession(authOptions);
     if (!session) throw 'you need to be logged in';
@@ -12,5 +13,17 @@ export async function saveProfile(formData: FormData) {
     const {
         username, displayName, bio
     } = Object.fromEntries(formData);
-    ProfileInfoModel.findOne({})
+    
+    // update or create depending on if it exists in db
+    // find by email
+    const profileInfoDoc = await ProfileInfoModel.findOne({email});
+    if (profileInfoDoc) { // if profile exists, save info
+        profileInfoDoc.set({username, displayName, bio});
+        profileInfoDoc.save();
+    } else { // else create new
+        await ProfileInfoModel.create({username, displayName, bio, email});
+    }
+
+    return true;
+    
 } 
--- src/components/ProfileInfoForm.tsx
+++ src/components/ProfileInfoForm.tsx
@@ -1,22 +1,30 @@
 'use client';
+
+import { saveProfile } from "@/actions/profileInfoActions";
+
 export default function ProfileInfoForm() {
+    async function handleFormAction(formData: FormData) {
+        const result = await saveProfile(formData);
+        console.log(result);
+    }
+
     return (
-        <form>
+        <form action={handleFormAction}>
             <div className="bg-gray-200 p-4 rounded-lg">
                 <div className="bg-gray-300 size-24 rounded-full p-4">Avatar</div>
                 <div>Cover Image</div>
             </div>
             <div>
                 <label className="block mt-4" htmlFor="usernameIn">Username</label>
-                <input id="usernameIn" type="text" placeholder="username"/>
+                <input name="username" id="usernameIn" type="text" placeholder="username"/>
             </div>
             <div>
                 <label className="block mt-4" htmlFor="displayNameIn">Display Name</label>
-                <input id="displayNameIn" type="text" placeholder="display name" />
+                <input name="displayName" id="displayNameIn" type="text" placeholder="display name" />
             </div>
             <div>
                 <label className="block mt-4" htmlFor="bioIn">Bio</label>
-                <textarea id="bioIn" name="" placeholder="bio"></textarea>
+                <textarea id="bioIn" name="bio" placeholder="bio"></textarea>
             </div>
             <div>
                 <button className="bg-blue-300 px-4 py-2 rounded-lg mt-4">
